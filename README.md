9b47786666c03b8430b5471db7028c66fd04614e
Jake Soenneker <jake@soenneker.com>
2023-04-11 13:31:40 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Standby...
+[![](https://img.shields.io/nuget/v/Soenneker.Extensions.Configuration.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Configuration/)
+[![](https://img.shields.io/github/actions/workflow/status/soenneker/soenneker.extensions.configuration/publish.yml?style=for-the-badge)](https://github.com/soenneker/soenneker.extensions.configuration/actions/workflows/publish.yml)
+[![](https://img.shields.io/nuget/dt/Soenneker.Extensions.Configuration.svg?style=for-the-badge)](https://www.nuget.org/packages/Soenneker.Extensions.Configuration/)
+
+# ![](https://user-images.githubusercontent.com/4441470/224455560-91ed3ee7-f510-4041-a8d2-3fc093025112.png) Soenneker.Extensions.Configuration
+### A collection of helpful IConfiguration extension methods
+
+## Installation
+
+```
+Install-Package Soenneker.Extensions.Configuration
+```
---
+++ src/ConfigurationExtension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Soenneker.Extensions.String;
+
+namespace Soenneker.Extensions.Configuration;
+
+/// <summary>
+/// A collection of helpful IConfiguration extension methods
+/// </summary>
+public static class ConfigurationExtension
+{
+    /// <summary>
+    /// Wraps IConfiguration.GetValue{T}(string), and if it's not present, it throws a <see cref="NullReferenceException"/>
+    /// </summary>
+    /// <exception cref="ArgumentNullException">Key is required</exception>
+    /// <exception cref="NullReferenceException">Cannot find the key in the configuration</exception>
+    public static T GetValueStrict<T>(this IConfiguration configuration, string key)
+    {
+        if (key.IsNullOrWhiteSpace())
+            throw new ArgumentNullException(nameof(key), $"The configuration key: '{key}' is invalid; it cannot be null or whitespace.");
+
+        var value = configuration.GetValue<T>(key);
+
+        if (value == null)
+            throw new NullReferenceException($"Could not retrieve the required configuration key: '{key}' ({typeof(T).Name}). Be sure the key is present in the IConfiguration used.");
+
+        return value;
+    }
+}

36f62182cbe84adc42b2376e12922c8de1712aa1
Jake Soenneker <jake@soenneker.com>
2025-03-18 08:08:13 -0500

    LogAll

--- src/ConfigurationExtension.cs
+++ src/ConfigurationExtension.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Soenneker.Extensions.String;
 
 namespace Soenneker.Extensions.Configuration;
@@ -25,5 +28,34 @@ public static class ConfigurationExtension
             throw new NullReferenceException($"Could not retrieve the required configuration key: '{key}' ({typeof(T).Name}). Be sure the key is present in the IConfiguration used.");
 
         return value;
+    }
+
+    /// <summary>
+    /// Logs all effective key-value pairs from the <see cref="IConfiguration"/> instance.
+    /// </summary>
+    /// <param name="configuration">The configuration instance to log.</param>
+    /// <param name="logger">The logger used to output configuration values.</param>
+    /// <remarks>
+    /// This method retrieves the final configuration values after all sources have been applied,
+    /// ignoring overridden or null values. It logs the settings in alphabetical order for readability.
+    /// </remarks>
+    public static void LogAll(this IConfiguration configuration, ILogger logger)
+    {
+        if (!configuration.GetValue<bool>("Log:StartupConfiguration"))
+            return;
+
+        IOrderedEnumerable<KeyValuePair<string, string?>> configValues = configuration
+                                                                         .AsEnumerable()
+                                                                         .Where(kvp => kvp.Value != null)
+                                                                         .OrderBy(kvp => kvp.Key);
+
+        logger.LogDebug("----- Start of effective IConfiguration -----");
+
+        foreach ((string key, string? value) in configValues)
+        {
+            logger.LogDebug("{key}={value}", key, value);
+        }
+
+        logger.LogDebug("----- End of effective IConfiguration -----");
     }
 }
